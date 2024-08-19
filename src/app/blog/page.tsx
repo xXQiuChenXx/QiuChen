34d33a9e2fc29fe28e2b90f51d74c1fda20966d8
Qiu Chen <51996408+xXQiuChenXx@users.noreply.github.com>
2024-08-20 00:08:18 +0800

    Update page.tsx

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const Blog = () => {
   return (
-    <div>Blog</div>
-  )
-}
+    <main className="grid items-center pt-6 md:py-8 py-4 gap-12 pb-10 md:pb-12">
+      Blog
+    </main>
+  );
+};
 
-export default Blog
+export default Blog;
