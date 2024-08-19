31ef52a532c41bb790fa88e5e98e603abff02cfb
Qiu Chen <51996408+xXQiuChenXx@users.noreply.github.com>
2024-08-20 00:08:05 +0800

    Update page.tsx

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const Projects = () => {
   return (
-    <div>Projects</div>
-  )
-}
+    <main className="grid items-center pt-6 md:py-8 py-4 gap-12 pb-10 md:pb-12">
+      Projects
+    </main>
+  );
+};
 
-export default Projects
+export default Projects;
