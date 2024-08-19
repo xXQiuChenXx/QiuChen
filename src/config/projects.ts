011b1299d34deaf05e8fb5b0b241af0337fd9593
Qiu Chen <51996408+xXQiuChenXx@users.noreply.github.com>
2024-08-20 00:07:48 +0800

    Create projects.ts

---
+++ src/config/projects.ts
@@ -0,0 +1,41 @@
+import { projectSchema } from "@/components/ProjectCard";
+
+
+export const projects: projectSchema[] = [
+    {
+      name: "project1",
+      description: "project1 description",
+      html_url: "https://github.com",
+      forks_count: 100,
+      homepage: "wsd",
+      language: "Typescript",
+      stargazers_count: 100,
+    },
+    {
+      name: "project2",
+      description: "project2 description",
+      html_url: "https://github.com",
+      forks_count: 100,
+      homepage: "wsd",
+      language: "Typescript",
+      stargazers_count: 100,
+    },
+    {
+      name: "project1",
+      description: "project3 description",
+      html_url: "https://github.com",
+      forks_count: 100,
+      homepage: "wsd",
+      language: "Typescript",
+      stargazers_count: 100,
+    },
+    {
+      name: "project4",
+      description: "project4 description",
+      html_url: "https://github.com",
+      forks_count: 100,
+      homepage: "wsd",
+      language: "Typescript",
+      stargazers_count: 100,
+    },
+  ];
