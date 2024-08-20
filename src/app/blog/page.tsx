f65bde9cedbe8025775e4be0f19a807e648cf284
Qiu Chen <51996408+xXQiuChenXx@users.noreply.github.com>
2024-08-21 01:38:29 +0800

    Blog Page

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,3 +1,5 @@
+import { BlogCard } from "@/components/BlogCard";
+import { documents } from "@/lib/documents";
 import { Metadata } from "next";
 import React from "react";
 
@@ -8,12 +10,17 @@ export const metadata: Metadata = {
 
 const Blog = () => {
   return (
-    <main className="grid items-center pt-6 md:py-8 py-4 gap-12 pb-10 md:pb-12">
+    <main className="pt-6 md:py-8 py-4 gap-12 pb-10 md:pb-12">
       <h1 className="font-semibold text-xl mb-2">Blog</h1>
       <p className="text-sm text-neutral-400 mb-4">
         My precious thoughts and inspirations.
       </p>
-      
+
+      <div className="flex flex-col gap-3">
+        {documents.map((d) => (
+          <BlogCard key={d.id} id={d.id} info={d.info} />
+        ))}
+      </div>
     </main>
   );
 };
