f8121eaa8c1b05a853f4f83dcceb67a6de39ffd6
Qiu Chen <51996408+xXQiuChenXx@users.noreply.github.com>
2024-08-21 00:26:27 +0800

    Add Project Page

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,9 +1,25 @@
 import React from "react";
+import { projects, projectTags } from "@/config/projects";
+import { ProjectCard } from "@/components/ProjectCard";
 
 const Projects = () => {
   return (
     <main className="grid items-center pt-6 md:py-8 py-4 gap-12 pb-10 md:pb-12">
-      Projects
+      {projectTags.map((tag) => {
+        return (
+          <div key={tag.id}>
+            <h1 className="font-semibold text-xl">{tag.name}</h1>
+            <p className="text-sm text-neutral-400 mb-2">{tag.description}</p>
+            <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
+              {projects
+                .filter((project) => project.tag === tag.id)
+                .map((project, i) => (
+                  <ProjectCard project={project} key={`${tag.id}-${i + 1}`} />
+                ))}
+            </div>
+          </div>
+        );
+      })}
     </main>
   );
 };
--- src/config/projects.ts
+++ src/config/projects.ts
@@ -1,41 +1,73 @@
-import { projectSchema } from "@/components/ProjectCard";
+type projectSchema = {
+  name: string;
+  tag: string;
+  description: string;
+  html_url: string;
+  stargazers_count: number;
+  forks_count: number;
+  homepage: string;
+  language: string; // Programming language
+};
 
+type projectTagSchema = {
+  id: string;
+  name: string;
+  description: string;
+};
+
+export const projectTags: projectTagSchema[] = [
+  {
+    id: "project",
+    name: "Projects",
+    description:
+      "Nice frameworks, libraries, web apps. My proud, high-quality harvests.",
+  },
+  {
+    id: "toy",
+    name: "My Toys",
+    description: "Fun projects",
+  },
+];
 
 export const projects: projectSchema[] = [
-    {
-      name: "project1",
-      description: "project1 description",
-      html_url: "https://github.com",
-      forks_count: 100,
-      homepage: "wsd",
-      language: "Typescript",
-      stargazers_count: 100,
-    },
-    {
-      name: "project2",
-      description: "project2 description",
-      html_url: "https://github.com",
-      forks_count: 100,
-      homepage: "wsd",
-      language: "Typescript",
-      stargazers_count: 100,
-    },
-    {
-      name: "project1",
-      description: "project3 description",
-      html_url: "https://github.com",
-      forks_count: 100,
-      homepage: "wsd",
-      language: "Typescript",
-      stargazers_count: 100,
-    },
-    {
-      name: "project4",
-      description: "project4 description",
-      html_url: "https://github.com",
-      forks_count: 100,
-      homepage: "wsd",
-      language: "Typescript",
-      stargazers_count: 100,
-    },
-  ];
+  {
+    name: "project1",
+    description: "project1 description",
+    html_url: "https://github.com",
+    forks_count: 100,
+    homepage: "wsd",
+    tag: "project",
+    language: "Typescript",
+    stargazers_count: 100,
+  },
+  {
+    name: "project2",
+    description: "project2 description",
+    html_url: "https://github.com",
+    forks_count: 100,
+    homepage: "wsd",
+    language: "Typescript",
+    tag: "project",
+    stargazers_count: 100,
+  },
+  {
+    name: "project1",
+    description: "project3 description",
+    html_url: "https://github.com",
+    forks_count: 100,
+    homepage: "wsd",
+    tag: "project",
+    language: "Typescript",
+    stargazers_count: 100,
+  },
+  {
+    name: "project4",
+    description: "project4 description",
+    html_url: "https://github.com",
+    forks_count: 100,
+    tag: "toy",
+    homepage: "wsd",
+    language: "Typescript",
+    stargazers_count: 100,
+  },
+];
