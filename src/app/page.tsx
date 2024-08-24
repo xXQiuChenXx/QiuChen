da4369bfa754162daf93383db1707b9ae4fb18b9
Qiu Chen <51996408+xXQiuChenXx@users.noreply.github.com>
2024-08-24 21:24:26 +0800

    Site Decorations

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,13 +6,13 @@ import { ProjectCard } from "@/components/ProjectCard";
 export default function Home() {
   return (
     <main className="grid items-center py-5 md:py-14 gap-12 pb-10 md:pb-12">
-      <p className="text-zinc-300 leading-loose">
+      <p className="dark:text-zinc-300 leading-loose prose-zinc">
         Hi there, I{`'`}m Tai Hong, an open sourcer from Malaysia. I enjoy
         involving in design and development of software and web projects,
-        enthusiastic about a broad range of topics.
-        During my leisure, I interested in creating contents, arts, and explore the power of Artificial Intelligence technology,
-        helping in today {`'s`} business and study field.
-        Currently I likes to build web applications using NextJS.
+        enthusiastic about a broad range of topics. During my leisure, I
+        interested in creating contents, arts, and explore the power of
+        Artificial Intelligence technology, helping in today{`'s`} business and
+        study field. Currently I likes to build web applications using NextJS.
       </p>
       <section className="space-y-4">
         <h2 className="prose prose-zinc text-xl font-semibold dark:prose-invert">
@@ -33,13 +33,15 @@ export default function Home() {
 }
 
 async function Projects() {
-  // const projects = await getProjects({ count: 4 })
-
   return (
     <>
-      {siteConfig.projects?.map((project) => (
-        <ProjectCard key={project.name} project={project} />
-      ))}
+      {siteConfig.projects?.length ? (
+        siteConfig.projects.map((project) => (
+          <ProjectCard key={project.name} project={project} />
+        ))
+      ) : (
+        <p>No projects found</p>
+      )}
     </>
   );
 }
