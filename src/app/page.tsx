cac59181ac6b50d6bada9121d64f79a5f33ce7df
Qiu Chen <51996408+xXQiuChenXx@users.noreply.github.com>
2024-09-22 12:00:33 +0800

    updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,8 @@ import { ProjectCard } from "@/components/ProjectCard";
 
 export default function Home() {
   return (
-    <main className="grid items-center py-5 md:py-14 gap-12 pb-10 md:pb-12">
-      <p className="dark:text-zinc-300 leading-loose prose-zinc">
+    <main className="grid items-center mt-10 md:mt-0 py-5 md:py-14 gap-12 pb-10 md:pb-12">
+      <p className="dark:text-zinc-300 prose-zinc text-sm/loose md:text-base/loose tracking-wide">
         Hi there, I{`'`}m Tai Hong, an open sourcer from Malaysia. I enjoy
         involving in design and development of software and web projects,
         enthusiastic about a broad range of topics. During my leisure, I
