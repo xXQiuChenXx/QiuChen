250703b3d421be8f729a0a539b5617b97e85adaa
Lau Tai Hong <lautaihong2004@gmail.com>
2025-05-04 05:15:14 +0800

    Update ProjectCard.tsx

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,6 +1,4 @@
 import Link from "next/link";
-import { formatNumber } from "@/lib/utils";
-import { StarIcon } from "lucide-react";
 import {
   Card,
   CardContent,
@@ -38,10 +36,6 @@ export function ProjectCard({ project }: ProjectCardProps) {
             />
             {project.language ?? "Unknown"}
           </div>
-          {/* <div className="flex items-center">
-            <StarIcon className="mr-1 size-3" aria-hidden="true" />
-            {formatNumber(project.stargazers_count)}
-          </div> */}
         </CardContent>
       </Link>
     </Card>
