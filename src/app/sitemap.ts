998cba31cad67eeab7111dd35355c3043c03b49c
Lau Tai Hong <lautaihong2004@gmail.com>
2025-09-06 21:37:23 +0800

    Update sitemap.ts

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,25 +1,43 @@
 import { getAllMdxFiles } from "@/lib/documents";
+import { resolve } from "node:url";
 import { MetadataRoute } from "next";
 import { siteConfig } from "@/config/site.config";
 import { mdxImports } from "@/config/posts";
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const getUrl = (v: string) => resolve(siteConfig.siteURL, v);
   const posts = await getAllMdxFiles();
-  const postRoutes = await Promise.all(
-    posts.map(async (post) => {
-      const mod = await mdxImports[post as keyof typeof mdxImports]();
-      const frontmatter = (mod as any).frontmatter;
-      return {
-        url: `${siteConfig.siteURL}/blog/${post}`,
-        lastModified: new Date(frontmatter.date).toISOString(),
-      };
-    })
-  );
 
-  const routes = ["", "blog"].map((route) => ({
-    url: `${siteConfig.siteURL}/${route}`,
-    lastModified: new Date().toISOString(),
-  }));
-
-  return [...routes, ...postRoutes];
+  return [
+    {
+      url: getUrl("/"),
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 1,
+    },
+    {
+      url: getUrl("/projects"),
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: getUrl("/blog"),
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.8,
+    },
+    ...(await Promise.all(
+      posts.map(async (post) => {
+        const mod = await mdxImports[post as keyof typeof mdxImports]();
+        const frontmatter = (mod as any).frontmatter;
+        return {
+          url: getUrl(`/blog/${post}`),
+          lastModified: new Date(frontmatter.date),
+          changeFrequency: "weekly" as const,
+          priority: 0.5,
+        };
+      })
+    )),
+  ];
 }
